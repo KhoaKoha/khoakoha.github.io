aa580899f7a6d89687db7203e83f1ae3372aa9ff
Koha <kohavn90@gmail.com>
2026-03-15 20:56:34 +0700

    init project

---
+++ script/command.js
@@ -0,0 +1,92 @@
+
+var password = "givemeyourpassword";
+var linkedin = "https://www.linkedin.com/in/anh-khoa-ton-that/";
+var github = "https://github.com/Khoakoha/";
+var email = 'mailto:forrest@fkcodes.com';
+
+whois = [
+  "<br>",
+  "Hello, I'm Khoa.",
+  "An QA Engineer that loves to automate testing and improve software quality.",
+  "I have experience with Linux, Windows, and various testing frameworks.",
+  "I still learning everyday for coding and ethical hacking.",
+  "<br>"
+];
+
+whoami = [
+  "Guest",
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/anh-khoa-ton-that' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/Khoakoha' + "</a>",
+  'Usage: Type <span class="command">linkedin</span> or <span class="command">github</span> to view my profiles.',
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "koaportfolio.netlify.app               In development",
+  '  <span class="inherit">A personal portfolio website buld with nextjs, three.js and tailwindcss.</span>',
+  "Portfolio                              In progress",
+  '  <span class="inherit">Build with nextjs, having BE with supabase integration.</span>',
+  "<br>"
+
+];
+
+
+
+help = [
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">whois</span>          Who is Forrest?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">banner</span>         Display the header',
+
+];
+
+banner = [
+  '<span class="index">Terminal style inspired by Forest Knight\'s template and ASCII art.</span>',
+
+"|-----------------------------------------------------------------------|",
+"|     _   _     |  Anh Khoa(Khoo)                       |     _   _     |",
+"|    (_) (_)    |  ttakhoa89@gmail.com                  |    (_) (_)    |",
+"|      <        |  We are what we repeatedly do.        |        >      |",
+"|   [\\_____/]   |  Excellence, then, is not an act,     |   [\\_____/]   |",
+"|       U       |  but a habit                          |       U       |",
+"|-----------------------------------------------------------------------|",
+  '<span class="color2">Welcome to funny interactive terminal.</span>',
+  "<span class=\"color2\">Type</span> <span class=\"command\">'help'</span><span class=\"color2\"></span> for the list of available commands.",
+  "If you can not typing, just click on the terminal and start typing! :)",
+  "Using up and down arrow keys to navigate through command history."
+];
+
+name= [
+  
+" __  __ _   _   ___    ___ ",
+"| |/ /| | | | / _ \\  / _ \\",
+"| ' / | |_| || | | || | | |",
+"| . \\ |  _  || |_| || |_| |",
+"|_|\\_\\|_| |_| \\___/  \\___/"
+];
+
+boot = [
+  "Initializing system...",
+  "Loading modules...",
+  "Starting terminal interface...",
+  "Rendering profile...",
+  "<br>"
+];
